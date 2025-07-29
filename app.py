40a0fb129470553556533cbb22dad8b71045f6a5
RedComet1027 <47207846+RedComet1027@users.noreply.github.com>
2025-07-29 06:27:50 +0000

    Re-indent all files and change Edit Portfolio button color

--- app.py
+++ app.py
@@ -7,69 +7,69 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    result_dicts = load_stocks_from_db()
-    return render_template('home.html', stocks=result_dicts)
+  result_dicts = load_stocks_from_db()
+  return render_template('home.html', stocks=result_dicts)
 
 
 @app.route('/hello')
 def hello():
-    return 'Hello, World!'
+  return 'Hello, World!'
 
 
 @app.route('/api/stock/<id>')
 def show_stock(id):
-    stock = load_stock_from_db(id)
-    return jsonify(stock)
+  stock = load_stock_from_db(id)
+  return jsonify(stock)
 
 @app.route('/new-portfolio')
 def new_portfolio():
-    stocks = load_stocks_from_db()
-    return render_template('new_portfolio.html', stocks=stocks)
+  stocks = load_stocks_from_db()
+  return render_template('new_portfolio.html', stocks=stocks)
 
 
 @app.route('/save-portfolio', methods=['POST'])
 def save_portfolio():
-    portfolio_name = request.form['portfolioName']
-    selected_stocks = request.form.getlist('selectedStocks')
-    save_portfolio_data_to_db(portfolio_name, selected_stocks)
-    return redirect('/')
+  portfolio_name = request.form['portfolioName']
+  selected_stocks = request.form.getlist('selectedStocks')
+  save_portfolio_data_to_db(portfolio_name, selected_stocks)
+  return redirect('/')
 
 @app.route('/list-portfolio')
 def list_portfolio():
-    portfolios = load_portfolios_from_db()
-    return render_template('list_portfolio.html', portfolios=portfolios)
+  portfolios = load_portfolios_from_db()
+  return render_template('list_portfolio.html', portfolios=portfolios)
 
 @app.route('/edit-portfolio')
 def edit_portfolio():
-    portfolio_name = request.args.get('name')
-    portfolio_stocks = load_portfolio_stocks_from_db(portfolio_name)
-    stocks = load_stocks_from_db()
+  portfolio_name = request.args.get('name')
+  portfolio_stocks = load_portfolio_stocks_from_db(portfolio_name)
+  stocks = load_stocks_from_db()
 
-    selected_stock_ids = {str(stock['id']) for stock in portfolio_stocks}
-    return render_template('edit_portfolio.html', 
-                           portfolio_name=portfolio_name, 
-                           stocks=stocks,
-                           selected_stock_ids=selected_stock_ids)
+  selected_stock_ids = {str(stock['id']) for stock in portfolio_stocks}
+  return render_template('edit_portfolio.html', 
+               portfolio_name=portfolio_name, 
+               stocks=stocks,
+               selected_stock_ids=selected_stock_ids)
 
 @app.route('/update-portfolio', methods=['POST'])
 def update_portfolio():
-    portfolio_name = request.form['portfolioName']
-    selected_stocks = request.form.getlist('selectedStocks')
-    
-    update_portfolio_data_to_db(portfolio_name, selected_stocks)
-    
-    return redirect('/list-portfolio')  # Redirect back to the list portfolio page
+  portfolio_name = request.form['portfolioName']
+  selected_stocks = request.form.getlist('selectedStocks')
+  
+  update_portfolio_data_to_db(portfolio_name, selected_stocks)
+  
+  return redirect('/list-portfolio')  # Redirect back to the list portfolio page
 
 @app.route('/delete-portfolio')
 def delete_portfolio():
-    portfolio_name = request.args.get('name')
-    delete_portfolio_from_db(portfolio_name)
-    
-    return redirect('/list-portfolio')  # Redirect back to the list portfolio page
-    
+  portfolio_name = request.args.get('name')
+  delete_portfolio_from_db(portfolio_name)
+  
+  return redirect('/list-portfolio')  # Redirect back to the list portfolio page
+  
 @app.route('/api/portfolio/<portfolio_name>/stocks')
 def get_portfolio_stocks(portfolio_name):
-    stocks = load_portfolio_stocks_from_db(portfolio_name)
-    return jsonify(stocks)
+  stocks = load_portfolio_stocks_from_db(portfolio_name)
+  return jsonify(stocks)
 
 app.run(host='0.0.0.0', port=81)
--- database.py
+++ database.py
@@ -13,75 +13,75 @@ conn_str = f'postgresql://{USERNAME}:{PASSWORD}@{HOST}/{DATABASE}?sslmode=requir
 engine = create_engine(conn_str)
 
 def load_stocks_from_db():
-    with engine.connect() as conn:
-        result = conn.execute(text("select * from stock"))
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from stock"))
 
-    # convert row objects to dicts
-    result_dicts = []
-    for row in result.all():
-        result_dicts.append(dict(row._mapping))
-    return result_dicts
+  # convert row objects to dicts
+  result_dicts = []
+  for row in result.all():
+    result_dicts.append(dict(row._mapping))
+  return result_dicts
 
 # for showing single stock by URL: /api/stock/1
 def load_stock_from_db(id):
-    with engine.connect() as conn:
-        # for SQLAchemy less than 2.0
-        #result = conn.execute(
-        #    text("select * from playing_with_neon where id = :val"), val=id)
+  with engine.connect() as conn:
+    # for SQLAchemy less than 2.0
+    #result = conn.execute(
+    #    text("select * from playing_with_neon where id = :val"), val=id)
 
-        # for SQLAchemy greater than 2.0, all params in dict
-        result = conn.execute(text("select * from stock where id = :val"),
-                              {"val": id})
+    # for SQLAchemy greater than 2.0, all params in dict
+    result = conn.execute(text("select * from stock where id = :val"),
+                {"val": id})
 
-        stock = result.all()
-        if len(stock) == 0:
-            # check if result is empty
-            return None
-        else:
-            # convert result (or row) object to a dict
-            return dict(stock[0]._mapping)
+    stock = result.all()
+    if len(stock) == 0:
+      # check if result is empty
+      return None
+    else:
+      # convert result (or row) object to a dict
+      return dict(stock[0]._mapping)
 
 def save_portfolio_data_to_db(portfolio_name, selected_stocks):
-    with engine.connect() as conn:
-        query = text("INSERT INTO user_folio (name, stock_tickers) VALUES (:name, :tickers)")
-        conn.execute(query, {"name": portfolio_name, "tickers": ','.join(selected_stocks)})
-        conn.commit()
+  with engine.connect() as conn:
+    query = text("INSERT INTO user_folio (name, stock_tickers) VALUES (:name, :tickers)")
+    conn.execute(query, {"name": portfolio_name, "tickers": ','.join(selected_stocks)})
+    conn.commit()
 
 def update_portfolio_data_to_db(portfolio_name, selected_stocks):
-    with engine.connect() as conn:
-        query = text("UPDATE user_folio SET stock_tickers = :tickers WHERE name = :name")
-        conn.execute(query, {"name": portfolio_name, "tickers": ','.join(selected_stocks)})
-        conn.commit()
+  with engine.connect() as conn:
+    query = text("UPDATE user_folio SET stock_tickers = :tickers WHERE name = :name")
+    conn.execute(query, {"name": portfolio_name, "tickers": ','.join(selected_stocks)})
+    conn.commit()
 
 def delete_portfolio_from_db(portfolio_name):
-    with engine.connect() as conn:
-        query = text("DELETE FROM user_folio WHERE name = :name")
-        conn.execute(query, {"name": portfolio_name})
-        conn.commit()
-        
+  with engine.connect() as conn:
+    query = text("DELETE FROM user_folio WHERE name = :name")
+    conn.execute(query, {"name": portfolio_name})
+    conn.commit()
+    
 def load_portfolios_from_db():
-    with engine.connect() as conn:
-        result = conn.execute(text("SELECT DISTINCT name FROM user_folio"))
-        return [dict(row._mapping) for row in result.all()]
+  with engine.connect() as conn:
+    result = conn.execute(text("SELECT DISTINCT name FROM user_folio"))
+    return [dict(row._mapping) for row in result.all()]
 
 def load_portfolio_stocks_from_db(portfolio_name):
-    with engine.connect() as conn:
-        # Get the stock IDs for the portfolio
-        portfolio = conn.execute(
-            text("SELECT stock_tickers FROM user_folio WHERE name = :name"),
-            {"name": portfolio_name}
+  with engine.connect() as conn:
+    # Get the stock IDs for the portfolio
+    portfolio = conn.execute(
+      text("SELECT stock_tickers FROM user_folio WHERE name = :name"),
+      {"name": portfolio_name}
+    ).first()
+    
+    if portfolio:
+      stock_ids = [int(id) for id in portfolio[0].split(',')]
+      stocks = []
+      # Get each stock individually
+      for stock_id in stock_ids:
+        result = conn.execute(
+          text("SELECT * FROM stock WHERE id = :id"),
+          {"id": stock_id}
         ).first()
-        
-        if portfolio:
-            stock_ids = [int(id) for id in portfolio[0].split(',')]
-            stocks = []
-            # Get each stock individually
-            for stock_id in stock_ids:
-                result = conn.execute(
-                    text("SELECT * FROM stock WHERE id = :id"),
-                    {"id": stock_id}
-                ).first()
-                if result:
-                    stocks.append(dict(result._mapping))
-            return stocks
-        return []
+        if result:
+          stocks.append(dict(result._mapping))
+      return stocks
+    return []
